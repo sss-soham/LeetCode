670589d8b57fd70ad8f0df33a0416442f25653cf
sss-soham <96948264+sss-soham@users.noreply.github.com>
2025-03-01 12:43:25 +0530

    Time: 0 ms (100%), Space: 30.4 MB (26.06%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,14 +3,11 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n;  // Handle cases where k > n
-        
-        while (k > 0) {
-            int temp = nums[n - 1];  // Store the last element before shifting
-            for (int i = n - 1; i > 0; i--) {
-                nums[i] = nums[i - 1];  // Shift each element to the right
-            }
-            nums[0] = temp;  // Move the last element to the first position
-            k--;  // Reduce k after one full shift
+
+        vector<int> temp(n);  // Extra space
+        for (int i = 0; i < n; i++) {
+            temp[(i + k) % n] = nums[i];  // Place elements at new positions
         }
+        nums = temp;  // Copy back
     }
 };
