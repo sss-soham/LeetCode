58100321dd1c761fed1b4809079b502d2f56900b
sss-soham <96948264+sss-soham@users.noreply.github.com>
2025-03-05 21:47:36 +0530

    Time: 4 ms (17.91%), Space: 11.7 MB (6.19%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int start = 0;
-        int end = s.length() - 1;
-        while(start <= end){
-            if(!isalnum(s[start])){
-                start++;
-                continue;
+        // int start = 0;
+        // int end = s.length() - 1;
+        // while(start <= end){
+        //     if(!isalnum(s[start])){
+        //         start++;
+        //         continue;
+        //     }
+        //     if(!isalnum(s[end])){
+        //         end--;
+        //         continue;
+        //     }
+        //     if(tolower(s[start]) != tolower(s[end])) return false;
+        //     else{
+        //         start++;
+        //         end--;
+        //     }
+        // }
+        // return true;
+
+        stack<char> str;
+        for(char c: s){
+            if(isalnum(c)){
+                str.push(tolower(c));
             }
-            if(!isalnum(s[end])){
-                end--;
-                continue;
-            }
-            if(tolower(s[start]) != tolower(s[end])) return false;
-            else{
-                start++;
-                end--;
+        }
+        for(char c: s){
+            if(isalnum(c)){
+                if(tolower(c) != str.top()) return false;
+                str.pop();
             }
         }
         return true;
