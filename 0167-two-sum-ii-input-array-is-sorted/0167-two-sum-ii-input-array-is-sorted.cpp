48773fc698b1736ac64d4f996ab7a5cd1d53c58f
Soham Santoshkumar Sakunde <96948264+sss-soham@users.noreply.github.com>
2025-03-06 00:55:54 +0530

    Time: 0 ms (100%), Space: 19.6 MB (5.52%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -4,9 +4,7 @@ public:
         int i = 0, j = nums.size() -1;
         while(i<j){
             int sum = nums[i] + nums[j];
-            if(sum == target){
-                return {i+1, j+1};
-            }
+            if(sum == target) return {i+1, j+1};
             else if(sum < target) i++;
             else j--;
         }
